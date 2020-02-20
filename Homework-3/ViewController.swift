806408afa13f1ec5638804910e9d0c462292e055
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-02-20 11:24:47 -0500

    Initial Commit

---
+++ Homework-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework-3
+//
+//  Created by Trevor Momeyer on 2/20/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Homework-3Tests/Homework_3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Homework_3Tests.swift
+//  Homework-3Tests
+//
+//  Created by Trevor Momeyer on 2/20/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import XCTest
+@testable import Homework_3
+
+class Homework_3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
