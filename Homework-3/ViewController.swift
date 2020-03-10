833be2b5014f32c838a8b7dd91a06b573d4e1725
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-03-10 12:25:28 -0400

    hi

--- Homework-3/ViewController.swift
+++ Homework-3/ViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var bunnyImage: UIImageView!
+    @IBOutlet weak var speedSlider: UISlider!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    
+    @IBAction func startstopButton(_ sender: Any) {
+        if(bunnyImage.isAnimating)
+        {
+            bunnyImage.stopAnimating()
+        } else
+        {
+            bunnyImage.startAnimating()
+        }
+    }
+    
+    @IBAction func setSpeed(_ sender: Any) {
+    bunnyImage.animationDuration=TimeInterval(4.0-speedSlider.value)
+        bunnyImage.startAnimating()
+    }
+    
+    
+    @IBAction func imageChange(_ sender: Any) {
+        backgroundImage.isHidden = true
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let hopAnimation: [UIImage] = [
+                   UIImage(named: "frame-1")!,
+                   UIImage(named: "frame-2")!,
+                   UIImage(named: "frame-3")!,
+                   UIImage(named: "frame-4")!,
+                   UIImage(named: "frame-5")!,
+                   UIImage(named: "frame-6")!,
+                   UIImage(named: "frame-7")!,
+                   UIImage(named: "frame-15")!,
+                   UIImage(named: "frame-16")!,
+                   UIImage(named: "frame-17")!,
+                   UIImage(named: "frame-18")!,
+                   UIImage(named: "frame-19")!,
+                   UIImage(named: "frame-20")!,
+                   ]
+        
+        bunnyImage.animationImages=hopAnimation
+        bunnyImage.animationDuration=1.0
     }
 
 
